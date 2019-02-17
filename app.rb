189d8a6cab9d235620fb955b0a638a40e328a622
Linda Chang <lindachang186@gmail.com>
2019-02-17 17:28:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     @new_phrase = PigLatinizer.new.piglatinize(params[:user_phrase])
+    
   end
 
 end
+
+"eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay
+ ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
+       expected "eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyw
+ay ourfay aysday ownay ithoutway akingtay away ishfay" to include "eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway etha
+y ulfGay ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay"
